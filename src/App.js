1bc7ed8308cfdefaab1a08d1823a6004502c544f
오민선 <minsunooh@gmail.com>
2024-04-27 23:03:55 +0900

    toDo1

--- src/App.js
+++ src/App.js
@@ -1,49 +1,28 @@
 import { useState, useEffect } from "react";
 
-function Hello() {
-//   function byFn() {
-//     console.log("Im gone :(");
-//   }
-//   function hiFn() {
-//     console.log("Im here :)");
-//     return byFn;
-//   }
-//  useEffect(hiFn, []);
-//   return <h1>Hello Ohgeo</h1>;
-// }
-
-function byFn() {
-  console.log("Im gone :(");
-}
-function hiFn() {
-  console.log("Im here :)");
-  return byFn;
-}
-useEffect(() => {
-  console.log("hi :)");
-  return () => console.log("bye :)");
-}, []); 
-
-// useEffect(function() {
-//   console.log("hi :)");
-//   return function() {
-//     console.log("bye :)");
-//   }
-// }, []); 
-
-return <h1>Hello Ohgeo</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing(prev => !prev); 
+  const [toDo, setToDo] = useState("");  
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault(); 
+    if(toDo === "") {
+      return;
+    } 
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  }; 
+  //console.log(toDos);
 
   return (
-    <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    <div>  
+      <h1>My To Dos({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..." />
+        <button>Add To Do</button>
+      </form>
     </div>
-  );
+  )  
 };
 
 export default App;
