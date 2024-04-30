7f2438069c56438141d1518465a4b7f86f0445a2
오민선 <minsunooh@gmail.com>
2024-04-30 17:56:30 +0900

    style 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
 
 function Home() {
@@ -21,16 +22,19 @@ function Home() {
     console.log(movies)
 
     return (
-        <div>
+        <div className={styles.container}>
             <h1>Movie List</h1>
             {loading ? (
-                <h1>Loading...</h1>
+                 <div className={styles.loader}>
+                    <span>Loading...</span>
+                </div>
             ) : (
-                <div>
+                <div className={styles.movies}>
                     {movies.map((movie) => (
                          <Movie 
                             key={movie.id}
                             id={movie.id}
+                            year={movie.year}
                             title={movie.title} 
                             summary={movie.summary} 
                             coverImg={movie.medium_cover_image} 
